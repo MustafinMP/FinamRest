8a1ad88e2e4e7978b1c2b5b46a954f52c74ed15e
musta <mustafinmp@yandex.ru>
2026-05-09 11:38:23 +0500

    Обновлен сервис генерации отчетов

--- finam_rest_py/services/report.py
+++ finam_rest_py/services/report.py
@@ -6,13 +6,13 @@ from finam_rest_py.services.base_service import AsyncBaseService
 
 
 class ReportService(AsyncBaseService):
-    async def init_report(self, date_begin: datetime, date_end: datetime, report_form) -> str:
+    async def init_report(self, date_begin: datetime, date_end: datetime, long_form: bool = False) -> str:
         """Инициирует формирование отчета на сервере
 
         Args:
             date_begin (datetime): левая граница периода отчета.
             date_end (datetime): правая граница периода отчета.
-            report_form (str): периода. Допустимые значения: 'short' или 'long'
+            long_form (bool): длинный формат отчета, по умолчанию False (короткий формат)
 
         Returns:
             report_id (int): ID отчета на сервере.
@@ -21,17 +21,10 @@ class ReportService(AsyncBaseService):
             FinamResponseFailureException: если произошла ошибка запроса к серверу.
         """
 
-        if report_form == 'short':
-            form = 'REPORT_FORM_SHORT'
-        elif report_form == 'long':
-            form = 'REPORT_FORM_LONG'
-        else:
-            raise ValueError(
-                f"Аргумент report_form имеет недопустимое значение {report_form} (требуется 'short' или 'long')")
         params = {
             'date_range.date_begin': date_begin.isoformat() + 'Z',
             'date_range.date_end': date_end.isoformat() + 'Z',
-            'report_form': form,
+            'report_form': 'REPORT_FORM_LONG' if long_form else 'REPORT_FORM_SHORT',
             'account_id': self._account_id
         }
         response = await self._session.post('report', params=params)
